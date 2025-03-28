4d71f6a65daa9df464689d7c1506e06fb0f53e3e
JoyOffere <j.offere@alustudent.com>
2025-03-28 09:48:42 +0000

    Update from container: Fri Mar 28 09:48:42 UTC 2025

--- push-to-repo.sh
+++ push-to-repo.sh
@@ -3,4 +3,4 @@ git config --global user.name "JoyOffere"
 git config --global user.email "j.offere@alustudent.com"
 git add .
 git commit -m "Update from container: $(date)"
-git push origin main
+git push https://${GIT_PAT}@github.com/JoyOffere/Formative-Project-Product-Catalog-API.git main
